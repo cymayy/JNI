da0546b2d3ac24a52f68b7bd6c393bc21bdd9ff1
cym <ym2581527>
2019-07-22 14:13:33 +0800

    添加日志打印方法

--- ccalljava/src/main/jni/CCallJava.c
+++ ccalljava/src/main/jni/CCallJava.c
@@ -6,6 +6,12 @@
 #include <jni.h>
 #include <stdio.h>
 
+#include <android/log.h>
+#define LOG_TAG "System.out"
+#define LOGD(...) __android_log_print(ANDROID_LOG_DEBUG, LOG_TAG, __VA_ARGS__)
+#define LOGI(...) __android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define LOGE(...) __android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+
 JNIEXPORT void JNICALL Java_com_mobile_ccalljava_JNI_callbackAdd
 (JNIEnv * env, jobject jobj){
     //1、得到字节码
@@ -17,10 +23,7 @@ JNIEXPORT void JNICALL Java_com_mobile_ccalljava_JNI_callbackAdd
     //3、实例化该类
     jint value = (*env)->CallIntMethod(env,jobject,jmethodIDs,99,1);
     printf("value=====%d",value);
+    LOGE("value===%d\n",value);
     //4、调用方法
-
-    const char *chars = "mNativeContext";
-    const char *chars2 = "J";
-    jfieldID  context = (*env)->GetFieldID(env,jclazz,chars,chars2);
 }
 
